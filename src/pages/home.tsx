fd6cf15e73dd8a93d62befb8a11024e4b1cb244f
Kellie <kellieh.exe@gmail.com>
2025-07-07 13:11:13 -0700

    set up a sample component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,16 @@
-import { ReactElement, useEffect } from 'react';
-
+import { ReactElement } from 'react';
+import MyComponent from '@components/component';
 
 export const Home = (): ReactElement => { 
-    useEffect(() => {
-        console.log("hello worldddd");
-    }, []);
-    
-    return <>
-        <header>
-            Hello World!!
-        </header></>
+
+    return (
+    <>
+            <MyComponent
+                title="Sample Title"
+                date="2025-07-07"
+                description="This is a sample description."
+            />
+    </> )
 }
 
 export default Home;
