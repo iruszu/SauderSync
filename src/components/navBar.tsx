93ed692b1fbf212250be745f952eed85034d61f1
Angela Cheng <acheng39@students.cs.ubc.ca>
2025-07-09 14:40:37 -0700

    navbar updated

---
+++ src/components/navBar.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { IconGauge, IconDoor, IconActivity, IconZoomIn, IconHelp } from '@tabler/icons-react'
+import { Box, NavLink } from '@mantine/core';
+import { Link } from 'react-router-dom';
+
+const data = [
+  { icon: IconGauge, label: 'Dashboard', description: 'Home Page', to: '/'
+  },
+  { icon: IconDoor,
+    label: 'Room Bookings', to:'room-bookings'
+  },
+  { icon: IconZoomIn, label: 'Opportunities', description: 'Explore opportunities',  to:'/opportunities' },
+  { icon: IconActivity, label: 'News', description: 'Get recent news updates', to: '/news' },
+  { icon: IconGauge, label: 'Settings', description: 'Manage your account', to: '/settings' },
+  { icon: IconHelp, label: 'Help', description: 'Get support', to: '/help' },
+  
+];
+
+
+function NavigationBar() {
+  const [active, setActive] = useState(0);
+
+  const items = data.map((item, index) => (
+    <NavLink
+      key={item.label}
+      component={item.to ? Link : 'div'}
+      to={item.to}
+      active={index === active}
+      label={item.label}
+      description={item.description}
+      leftSection={<item.icon size={16} stroke={1.5} />}
+      onClick={() => setActive(index)}
+      color="green"
+    />
+  ));
+
+  return <Box w={220}>{items}</Box>;
+}
+
+export default NavigationBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Debugger from './debugger';
 import BrandingPage from './branding';
 import Home from './home';
+import Opportunities from './opportunities';
 
 export const RootRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/opportunities" element={<Opportunities/>} />
       <Route path="/debugger" element={<Debugger />} />
       <Route path="/branding" element={<BrandingPage />} />
       <Route path="/*" element={<Navigate to="/" />} />
