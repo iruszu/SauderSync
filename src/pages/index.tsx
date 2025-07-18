6a6a346e616628a99aeed20a1eb071fec00fb621
Angela Cheng <acheng39@students.cs.ubc.ca>
2025-07-18 14:05:13 -0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import CreateOpportunity from './createOpportunity';
 import Home from './home';
 import Opportunities from './opportunities';
 import { AuthenticationForm } from './login/AuthenticationForm';
+import RoomBookings from './roomBookings';
 
 export const RootRoutes = () => {
   return (
@@ -11,6 +12,7 @@ export const RootRoutes = () => {
       <Route path="/opportunities" element={<Opportunities/>} />
       <Route path="/authentication" element={<AuthenticationForm/>} />
       <Route path="/createOpportunity" element={<CreateOpportunity />} />
+      <Route path="/roomBookings" element={<RoomBookings/>} />
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   );
