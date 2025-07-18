48d9bbd5f7ccad6d03c9d487cf7adce72c0f868a
Kellie <kellieh.exe@gmail.com>
2025-07-18 13:43:58 -0700

    fixed index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import CreateOpportunity from './createOpportunity';
-<<<<<<< HEAD
-import BrandingPage from './branding';
-=======
 import Home from './home';
->>>>>>> d9db4fc5fecd7afa8bdf682fe2c7e632e2c9306b
 import Opportunities from './opportunities';
 import { AuthenticationForm } from './login/AuthenticationForm';
-import Home from './home';
 
 
 export const RootRoutes = () => {
@@ -17,11 +12,6 @@ export const RootRoutes = () => {
       <Route path="/opportunities" element={<Opportunities/>} />
       <Route path="/authentication" element={<AuthenticationForm/>} />
       <Route path="/createOpportunity" element={<CreateOpportunity />} />
-<<<<<<< HEAD
-      <Route path="/branding" element={<BrandingPage />} />
-=======
-      <Route path="/homeNew" element={<HomeNew/>} />
->>>>>>> d9db4fc5fecd7afa8bdf682fe2c7e632e2c9306b
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   );
