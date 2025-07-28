ad605766f23c603270d8957eb61be97c0c9e1753
Kellie <kellieh.exe@gmail.com>
2025-07-28 11:47:54 -0700

    removed old room booking page

--- src/pages/roomBookings.tsx
+++ src/pages/roomBookings.tsx
@@ -1,8 +1,5 @@
 import { ReactElement, useMemo, useState } from 'react';
 import { Flex, Title, Box, Select } from '@mantine/core';
-import { DateTab } from '@components/roomBookings/DateSelector';
-import RoomTabs from '@components/roomBookings/RoomTabs';
-import { ViewTab } from '@components/roomBookings/ViewSelector';
 
 export const RoomBookings = (): ReactElement => {
   return (
@@ -10,10 +7,7 @@ export const RoomBookings = (): ReactElement => {
       <Box p="md" style={{ flex: 1 }}>
         <h1> Book a Room </h1>
         <Flex mb="md" gap="md">
-          <RoomTabs />
-          <ViewTab />
           <h3>Date</h3>
-          <DateTab />
         </Flex>
       </Box>
     </Flex>
